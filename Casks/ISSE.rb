9fccb4bc9b7072bcfeae3601642bc227c183bf7c
airtoxin <tahirimaka6@gmail.com>
2014-06-16 02:18:17 +0900

    update

--- Casks/ISSE.rb
+++ Casks/ISSE.rb
@@ -2,6 +2,6 @@ class ISSE < Cask
   url 'http://downloads.sourceforge.net/project/isse/alpha/0.2.0/ISSE-Setup-v0.2.0.dmg?r=http%3A%2F%2Fisse.sourceforge.net%2Fdownload-link-osx.html&ts=1402845944&use_mirror=cznic'
   homepage 'http://isse.sourceforge.net/'
   version '0.2.0'
-  sha1 '8e077da3427a8dcf39566fea4f47e226acf08fdd'
+  sha256 '74cb27d64405a31baa5d1c4c5f8689d671c7ce5eb1bf986ec6db16a066aae940'
   link 'ISSE.app'
 end
--- Casks/nuwk.rb
+++ Casks/nuwk.rb
@@ -2,6 +2,6 @@ class Nuwk < Cask
   url 'http://download1411.mediafire.com/k6culxbm720g/xy053b3qmqyx7w7/nuwk.1.0.0.zip'
   homepage 'http://codeb.it/nuwk/'
   version '1.0.0'
-  sha1 '25c9fc028408faef99b82debbdf4125333218850'
+  sha256 '13c256f1cbe30c01672e9b597218fac243c7c6d727587e4d5d27b4b5994f1f9d'
   link 'Nuwk!.app'
 end

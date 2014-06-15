e20f9217d9eb68fcbf7f703a05a0769c97e58e22
airtoxin <tahirimaka6@gmail.com>
2014-06-16 02:21:05 +0900

    update

--- Casks/brettspielwelt.rb
+++ Casks/brettspielwelt.rb
@@ -2,6 +2,6 @@ class BrettspielWelt < Cask
   url 'http://www.brettspielwelt.de/Data/brettspielwelt.dmg'
   homepage 'http://www.brettspielwelt.de/'
   version '1.0'
-  sha1 '93bcfad3c993756a97af149a5b758a276d643bfcf3e4297f5a87315b5dce8a6b'
-  link 'BrettspielWelt'
+  sha256 '93bcfad3c993756a97af149a5b758a276d643bfcf3e4297f5a87315b5dce8a6b'
+  link 'BrettspielWelt/'
 end
